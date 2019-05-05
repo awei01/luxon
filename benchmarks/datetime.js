7f5a8c0529d4d216791423b7b70690f75e48b614
Isaac Cambron <isaac@isaaccambron.com>
2019-05-04 20:50:29 -0400

    wip remove invalid

--- benchmarks/datetime.js
+++ benchmarks/datetime.js
@@ -17,11 +17,11 @@ suite
   .add("DateTime.local with numbers", () => {
     DateTime.local(2017, 5, 15);
   })
-  .add("DateTime.fromString", () => {
-    DateTime.fromString("1982/05/25 09:10:11.445", "yyyy/MM/dd HH:mm:ss.SSS");
+  .add("DateTime.fromFormat", () => {
+    DateTime.froFormat("1982/05/25 09:10:11.445", "yyyy/MM/dd HH:mm:ss.SSS");
   })
-  .add("DateTime.fromString with zone", () => {
-    DateTime.fromString("1982/05/25 09:10:11.445", "yyyy/MM/dd HH:mm:ss.SSS", {
+  .add("DateTime.froFormat with zone", () => {
+    DateTime.froFormat("1982/05/25 09:10:11.445", "yyyy/MM/dd HH:mm:ss.SSS", {
       zone: "America/Los_Angeles"
     });
   })
